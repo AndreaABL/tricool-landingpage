7d3338e1285ee3534e4bde79df8ac778ea28e84a
Andrea <abrioneslopez0@gmail.com>
2026-01-30 16:26:00 -0300

    actualizacion fotos coaches

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -32,6 +32,8 @@ import gallery19 from "./assets/gallery-19.jpeg";
 import gallery20 from "./assets/gallery-20.jpeg";
 
 import miguelOyarce from "./assets/coaches/miguel-oyarce.jpeg";
+import leonelCid from "./assets/coaches/leonel-cid.jpeg";
+import hansSalas from "./assets/coaches/hans-salas.jpeg";
 
 import hero1 from "./assets/hero-1.jpeg";
 import hero2 from "./assets/hero-2.jpeg";
